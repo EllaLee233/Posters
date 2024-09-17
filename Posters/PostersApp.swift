a69353a5c8da760d253017cd92c297418aabc46e
EllaLee233 <lmlll2293068536@gmail.com>
2024-09-16 20:25:55 -0400

    Initial Commit

---
+++ Posters/PostersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostersApp.swift
+//  Posters
+//
+//  Created by apple on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PostersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
